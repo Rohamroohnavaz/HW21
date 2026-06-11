2932878f0d25291aaa6ed202e8f8e43068220a48
Roham Roohnavaz <roohnavazroham12@gmail.com>
2026-06-11 12:08:29 +0330

    Check Errors

--- HW21.Infrastructure/Data/AppDbContext.cs
+++ HW21.Infrastructure/Data/AppDbContext.cs
@@ -12,6 +12,10 @@ namespace HW21.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext()
+        {
+            
+        }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
         {
             
